97ae01b69f3844a0c75d497e8b7e357f6c5afdf2
Piyush Kumar <piyushkumg@gmail.com>
2020-04-11 00:43:36 +0530

    cpufreq: intel_pstate: adjust EPP values
    performance : 0 -> 32
    balance_power : 192 -> 160

--- arch/x86/include/asm/msr-index.h
+++ arch/x86/include/asm/msr-index.h
@@ -413,9 +413,9 @@
 #define HWP_MAX_PERF(x) 		((x & 0xff) << 8)
 #define HWP_DESIRED_PERF(x)		((x & 0xff) << 16)
 #define HWP_ENERGY_PERF_PREFERENCE(x)	(((unsigned long long) x & 0xff) << 24)
-#define HWP_EPP_PERFORMANCE		0x00
+#define HWP_EPP_PERFORMANCE		0x20
 #define HWP_EPP_BALANCE_PERFORMANCE	0x80
-#define HWP_EPP_BALANCE_POWERSAVE	0xC0
+#define HWP_EPP_BALANCE_POWERSAVE	0xA0
 #define HWP_EPP_POWERSAVE		0xFF
 #define HWP_ACTIVITY_WINDOW(x)		((unsigned long long)(x & 0xff3) << 32)
 #define HWP_PACKAGE_CONTROL(x)		((unsigned long long)(x & 0x1) << 42)
