6fd1f15dbf4dbc2d8adc0ec5b1c322cf742fdc60
Alfred Chen <cchalpha@gmail.com>
2022-05-19 12:41:20 +0000

    sched/alt: [Sync] 04d4e665a609 sched/isolation: Use single feature type while referring to housekeeping cpumask

--- kernel/sched/alt_core.c
+++ kernel/sched/alt_core.c
@@ -1018,13 +1018,13 @@ int get_nohz_timer_target(void)
 	struct cpumask *mask;
 	const struct cpumask *hk_mask;
 
-	if (housekeeping_cpu(cpu, HK_FLAG_TIMER)) {
+	if (housekeeping_cpu(cpu, HK_TYPE_TIMER)) {
 		if (!idle_cpu(cpu))
 			return cpu;
 		default_cpu = cpu;
 	}
 
-	hk_mask = housekeeping_cpumask(HK_FLAG_TIMER);
+	hk_mask = housekeeping_cpumask(HK_TYPE_TIMER);
 
 	for (mask = per_cpu(sched_cpu_topo_masks, cpu) + 1;
 	     mask < per_cpu(sched_cpu_topo_end_mask, cpu); mask++)
@@ -1033,7 +1033,7 @@ int get_nohz_timer_target(void)
 				return i;
 
 	if (default_cpu == -1)
-		default_cpu = housekeeping_any_cpu(HK_FLAG_TIMER);
+		default_cpu = housekeeping_any_cpu(HK_TYPE_TIMER);
 	cpu = default_cpu;
 
 	return cpu;
@@ -4062,7 +4062,7 @@ static void sched_tick_start(int cpu)
 	int os;
 	struct tick_work *twork;
 
-	if (housekeeping_cpu(cpu, HK_FLAG_TICK))
+	if (housekeeping_cpu(cpu, HK_TYPE_TICK))
 		return;
 
 	WARN_ON_ONCE(!tick_work_cpu);
@@ -4082,7 +4082,7 @@ static void sched_tick_stop(int cpu)
 {
 	struct tick_work *twork;
 
-	if (housekeeping_cpu(cpu, HK_FLAG_TICK))
+	if (housekeeping_cpu(cpu, HK_TYPE_TICK))
 		return;
 
 	WARN_ON_ONCE(!tick_work_cpu);
@@ -7119,7 +7119,7 @@ static void sched_init_topology_cpumask(void)
 void __init sched_init_smp(void)
 {
 	/* Move init over to a non-isolated CPU */
-	if (set_cpus_allowed_ptr(current, housekeeping_cpumask(HK_FLAG_DOMAIN)) < 0)
+	if (set_cpus_allowed_ptr(current, housekeeping_cpumask(HK_TYPE_DOMAIN)) < 0)
 		BUG();
 	current->flags &= ~PF_NO_SETAFFINITY;
 
