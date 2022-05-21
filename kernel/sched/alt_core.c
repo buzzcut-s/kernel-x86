b86ee7fdd436f66a0aba401db9c175478d24be8c
Alfred Chen <cchalpha@gmail.com>
2022-05-21 07:43:39 +0000

    sched/alt: [Sync] fa2c3254d7cf sched/tracing: Don't re-read p->state when emitting sched_switch event

--- kernel/sched/alt_core.c
+++ kernel/sched/alt_core.c
@@ -3430,7 +3430,7 @@ static struct rq *finish_task_switch(struct task_struct *prev)
 {
 	struct rq *rq = this_rq();
 	struct mm_struct *mm = rq->prev_mm;
-	long prev_state;
+	unsigned int prev_state;
 
 	/*
 	 * The previous task will have left us with a preempt_count of 2
@@ -4595,7 +4595,7 @@ static void __sched notrace __schedule(unsigned int sched_mode)
 
 		psi_sched_switch(prev, next, !task_on_rq_queued(prev));
 
-		trace_sched_switch(sched_mode & SM_MASK_PREEMPT, prev, next);
+		trace_sched_switch(sched_mode & SM_MASK_PREEMPT, prev, next, prev_state);
 
 		/* Also unlocks the rq: */
 		rq = context_switch(rq, prev, next);
